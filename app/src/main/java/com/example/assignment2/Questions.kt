af8cc2b69d9d2bd58c4415e4ff2d5701e531316d
RoseJosh <ST10486426@rcconnect.edu.za>
2025-04-30 13:32:33 +0200

    add Questions page

--- app/src/main/java/com/example/assignment2/Questions.kt
+++ app/src/main/java/com/example/assignment2/Questions.kt
@@ -1,16 +1,22 @@
 package com.example.assignment2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.assignment2.MainActivity
 import com.example.assignment2.ui.theme.Assignment2Theme
 
 class Questions : ComponentActivity() {
@@ -19,29 +25,20 @@ class Questions : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assignment2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(start = 30.dp, top = 80.dp, end = 30.dp)
+                ) {
+                    Button(onClick = {
+                        val next = Intent(this@Questions, MainActivity::class.java);
+                        startActivity(next)
+                    }) {
+                        Text(text = "START")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Assignment2Theme {
-        Greeting("Android")
     }
 }
